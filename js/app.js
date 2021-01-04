b26e26d1b4542b22f9a5f4472ccc2674c69f4ec9
Deyaan <65954790+Deyaan@users.noreply.github.com>
2021-01-04 16:40:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function LoginUser() {
 
   firebase.auth().signInWithEmailAndPassword(email,password).then(function(){
     alert('User login successfully');
-    window.location.href='https://deyaan.github.io/EV-MAIN/'
+    window.location.href='https://deyaan.github.io/EV-MAP/'
 
   }).catch(function(error){
     var errorcode=error.code;
